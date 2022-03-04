f6677b471f03e35c34f042231cd62fdd2a0fee69
J-Babi <88941451+J-Babi@users.noreply.github.com>
2022-03-04 15:58:41 -0500

    Update UserRepository.java

--- src/main/java/com/mt/springmongo/UserRepository.java
+++ src/main/java/com/mt/springmongo/UserRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * Repository for saving new and retrieving existing {@link User}s from MongoDB
- *
- * @author Mithun
  */
 public interface UserRepository extends PagingAndSortingRepository<User, String> {
 }
