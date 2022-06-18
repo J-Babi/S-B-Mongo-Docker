50eac0d4aac20a3ecb245d8ce1c81fe5556022ab
J-Babi <88941451+J-Babi@users.noreply.github.com>
2022-06-18 14:48:04 -0400

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.location
 }
 
-resource "azurerm_kubernetes_cluster" "AKS_dev" {
+resource "azurerm_kubernetes_cluster" "k8s_dev" {
   name                = "${var.cluster_name}_${var.environment}"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
